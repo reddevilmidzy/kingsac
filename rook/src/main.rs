e17c790f9055ade37577eff84873576108a12563
reddevilmidzy <midzy0228@gmail.com>
2025-07-17 02:03:39 +0900

    add test

--- rook/src/main.rs
+++ rook/src/main.rs
@@ -14,11 +14,11 @@ impl Team {
 }
 struct User {
     username: String,
-    age: u8,
+    age: u16,
 }
 
 impl User {
-    fn new(username: String, age: u8) -> Self {
+    fn new(username: String, age: u16) -> Self {
         Self { username, age }
     }
 
@@ -30,7 +30,7 @@ impl User {
         &self.username
     }
 
-    fn get_age(&self) -> u8 {
+    fn get_age(&self) -> u16 {
         self.age
     }
 }
@@ -50,6 +50,7 @@ mod tests {
 
     #[test]
     fn test_user_new() {
+        std::thread::sleep(std::time::Duration::from_secs(1));
         let user = User::new("Redddy".to_string(), 20);
         assert_eq!(user.get_username(), "Redddy");
         assert_eq!(user.get_age(), 20);
@@ -57,7 +58,22 @@ mod tests {
 
     #[test]
     fn test_user_is_adult() {
+        std::thread::sleep(std::time::Duration::from_secs(1));
         let user = User::new("Redddy".to_string(), 20);
         assert_eq!(user.is_adult(), true);
+    }
+
+    #[test]
+    fn test_team_new() {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        let team = Team::new(
+            "사자보이즈".to_string(),
+            vec![
+                User::new("진우".to_string(), 400),
+                User::new("애비".to_string(), 20),
+            ],
+        );
+        assert_eq!(team.name, "사자보이즈");
+        assert_eq!(team.members.len(), 2);
     }
 }
