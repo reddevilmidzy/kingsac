fdaa741bb1a9c197f037ee4c9ec8a3cd4647fbb0
reddevilmidzy <midzy0228@gmail.com>
2025-07-16 21:54:43 +0900

    add  code

--- rook/src/main.rs
+++ rook/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-	println!("??");
+    println!("??");
+}
+
+struct User {
+    username: String,
+    age: u8,
+}
+
+impl User {
+    fn new(username: String, age: u8) -> Self {
+        Self { username, age }
+    }
+
+    fn get_username(&self) -> &str {
+        &self.username
+    }
+
+    fn get_age(&self) -> u8 {
+        self.age
+    }
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
